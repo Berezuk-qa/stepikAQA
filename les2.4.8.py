182d4729cfb2b7579b0cd772039ce98758eb445b
Berezuk-qa <bereziuk.o@i.ua>
2023-10-04 21:56:57 +0300

    Add file

--- les2.4.8.py
+++ les2.4.8.py
@@ -1,17 +1,39 @@
+#чекоэмо поки буде ціна 100 потім клікаємо буук, долі обраховуэмо результат та клікаємо кнопку
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
-
+import time
 browser = webdriver.Chrome()
 
-browser.get("http://suninjuly.github.io/wait2.html")
+browser.get("http://suninjuly.github.io/explicit_wait2.html")
 
 # говорим Selenium проверять в течение 5 секунд, пока кнопка не станет кликабельной
-button = WebDriverWait(browser, 14).until(
-        EC.element_to_be_clickable((By.ID, "verify"))
-    )
+WebDriverWait(browser, 14).until(EC.text_to_be_present_in_element((By.ID, "price"), "$100"))
+button = browser.find_element(By.ID, "book")
 button.click()
-message = browser.find_element(By.ID, "verify_message")
 
-assert "successful" in message.text
+import math
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+
+x_element = browser.find_element(By.XPATH, '//*[@id="input_value"]')
+x = x_element.text
+y = calc(x)
+
+input1 = browser.find_element(By.ID, "answer")
+input1.send_keys(y)
+
+sub = browser.find_element(By.ID, "solve")
+sub.click()
+print(browser.switch_to.alert.text)
+
+
+    # успеваем скопировать код за 30 секунд
+time.sleep(30)
+    # закрываем браузер после всех манипуляций
+browser.quit()
+
+# не забываем оставить пустую строку в конце файла
+
