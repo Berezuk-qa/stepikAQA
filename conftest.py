b34e5b0588589cf968b05e6b44f2a9751c96c664
Berezuk-qa <bereziuk.o@i.ua>
2023-10-27 13:29:07 +0300

    коміт 27.10

--- conftest.py
+++ conftest.py
@@ -1,32 +1,23 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options as ChromeOptions
-from selenium.webdriver.safari.options import Options as SafariOptions
-
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default='chrome', help='Choose browser: chrome or safari')
-    parser.addoption('--user_language', action='store', default='ru', help='Choose language: ru or en')
+    parser.addoption('--browser_name', action='store', default=None,
+                     help="Choose browser: chrome or firefox")
+
 
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
-    user_language = request.config.getoption("user_language")
     browser = None
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
-        chrome_options = ChromeOptions()
-        chrome_options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-        browser = webdriver.Chrome(options=chrome_options)
-    elif browser_name == "safari":
-        print("\nstart safari browser for test..")
-        safari_options = SafariOptions()
-        # кажется никто не знает как это в Safari сделать
-        browser = webdriver.Safari(options=safari_options)
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
     else:
-        raise pytest.UsageError("--browser_name should be chrome or safari")
-    browser.implicitly_wait(5)
-
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     print("\nquit browser..")
     browser.quit()
